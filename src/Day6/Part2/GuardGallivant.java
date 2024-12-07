a1a5ec636f107bf5dda2fb26baa5815f0d147d31
Bojan Popovski <bojpopovski2002@gmail.com>
2024-12-07 09:24:52 +0100

    Day 6 finished

--- src/Day6/Part2/GuardGallivant.java
+++ src/Day6/Part2/GuardGallivant.java
@@ -1,6 +1,8 @@
-package Day6.Part1;
+package Day6.Part2;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -28,21 +30,24 @@ class GuardGallivant {
 
         char[][] map = inputMap.toArray(new char[0][]);
 
-        int guardY = -1, guardX = -1;
+        int guardStartY = -1, guardStartX = -1;
         for (int i = 0; i < map.length; i++) {
             for (int j = 0; j < map[i].length; j++) {
                 if (map[i][j] == '^') {
-                    guardY = i;
-                    guardX = j;
+                    guardStartY = i;
+                    guardStartX = j;
                     break;
                 }
             }
-            if (guardY != -1) break;
+            if (guardStartY != -1) break;
         }
 
         int[][] directions = {{-1, 0}, {0, 1}, {1, 0}, {0, -1}};
         int dirIndex = 0;
+
+        // Part1
         int counter = 1;
+        int guardY = guardStartY, guardX = guardStartX;
 
         while (true) {
             int nextY = guardY + directions[dirIndex][0];
@@ -64,6 +69,48 @@ class GuardGallivant {
         }
 
         System.out.println(counter);
-    }
 
+        // Part2
+        int counter2 = 0;
+
+        for (int i = 1; i < map.length - 1; i++) {
+            for (int j = 1; j < map[i].length - 1; j++) {
+                if (map[i][j] != 'X') continue;
+
+                char[][] mapCopy = new char[map.length][];
+                for (int k = 0; k < map.length; k++) {
+                    mapCopy[k] = map[k].clone();
+                }
+                mapCopy[i][j] = '#';
+
+                guardY = guardStartY;
+                guardX = guardStartX;
+                dirIndex = 0;
+
+                while (true) {
+                    int nextY = guardY + directions[dirIndex][0];
+                    int nextX = guardX + directions[dirIndex][1];
+
+                    if (mapCopy[nextY][nextX] == '.' || mapCopy[nextY][nextX] == 'X' || mapCopy[nextY][nextX] == '^') {
+                        mapCopy[nextY][nextX] = '1';
+                        guardY = nextY;
+                        guardX = nextX;
+                    } else if (mapCopy[nextY][nextX] == '#') {
+                        dirIndex = (dirIndex + 1) % directions.length;
+                    } else if (mapCopy[nextY][nextX] == 'E') {
+                        break;
+                    } else if (mapCopy[nextY][nextX] == '4') {
+                        counter2++;
+                        break;
+                    } else {
+                        mapCopy[nextY][nextX]++;
+                        guardY = nextY;
+                        guardX = nextX;
+                    }
+                }
+            }
+        }
+
+        System.out.println(counter2);
+    }
 }
